cd2e1cf259916309584d6b734a50e7655d5a8271
Gabr1313 <gabriele.baietta03@gmail.com>
2024-01-20 23:16:25 +0100

    albedo and normal from src/draw.c

--- c/src/draw.c
+++ c/src/draw.c
@@ -13,7 +13,9 @@
 void shoot_a_pixel(Float3* pixel_to_update, const int sqrt_ray_per_pixel,
 				   const Ray3* upper_left, const Float3* d_x, const Float3* d_y,
 				   const ObjectVec* objects, const float max_bounces,
-				   const Float3* background);
+				   const Float3* background,
+				   Float3 (*tracer)(const Ray3*, const ObjectVec*, const int,
+									const Float3*));
 
 int int_min(int a, int b) { return a < b ? a : b; }
 
@@ -50,7 +52,7 @@ void shoot_and_draw(const InputData* input_data) {
 	fwrite(header, sizeof(char), heder_len, stdout);
 
 	srand(time(NULL));
-	fprintf(stderr, "0 / %d", number_of_updates);
+	fprintf(stderr, "ray tracing: 0 / %d", number_of_updates);
 	for (int nou = 1; nou <= number_of_updates; nou++) {
 		const float to_multiply = 255.0f / (nou * ray_per_pixel);
 		Ray3 row = *upper_left;
@@ -58,7 +60,7 @@ void shoot_and_draw(const InputData* input_data) {
 			Ray3 col = row;
 			for (int j = 0; j < width; j++, idx++) {
 				shoot_a_pixel(&pixel_sum[idx], sqrt_ray_per_pixel, &col, d_x,
-							  d_y, objects, max_bounces, background);
+							  d_y, objects, max_bounces, background, trace_ray);
 				buffer[idx_buffer++] =
 					int_min(pixel_sum[idx].x * to_multiply, 255);
 				buffer[idx_buffer++] =
@@ -73,22 +75,77 @@ void shoot_and_draw(const InputData* input_data) {
 		fseek(stdout, heder_len, SEEK_SET);
 		fwrite(buffer, sizeof(unsigned char), content_len, stdout);
 		fflush(stdout);
-		fprintf(stderr, "\r%d / %d", nou, number_of_updates);
+		fprintf(stderr, "\rray tracing: %d / %d", nou, number_of_updates);
 	}
 	fprintf(stderr, "\n");
 
 	if (save_floats) {
-		const float to_multiply = 1.0f / (number_of_updates * ray_per_pixel);
+		char* filename = "color.pfm";
+		fprintf(stderr, "%s: 0 / 1", filename);
+		float to_multiply = 1.0f / (number_of_updates * ray_per_pixel);
 		for (int i = 0; i < total_pixel; i++)
 			float3_mul_eq(&pixel_sum[i], to_multiply);
-		FILE* file = fopen("floats.pfm", "wb");
+		FILE* file = fopen("color.pfm", "wb");
 		if (file == NULL) {
-			fprintf(stderr, "Error: can't open file floats.bin\n");
+			fprintf(stderr, "Error: can't open file %s\n", filename);
 			exit(-1);
 		}
 		fprintf(file, "PF\n%d %d\n-1.0\n", width, height);
 		fwrite(pixel_sum, sizeof(Float3), total_pixel, file);
 		fclose(file);
+		fprintf(stderr, "\r%s: 1 / 1\n", filename);
+
+		filename = "albedo.pfm";
+		fprintf(stderr, "%s: 0 / 1", filename);
+		to_multiply = 1.0f / ray_per_pixel;
+		Ray3 row = *upper_left;
+		for (int i = 0, idx = 0; i < height; i++) {
+			Ray3 col = row;
+			for (int j = 0; j < width; j++, idx++) {
+				pixel_sum[idx] = float3_new(0, 0, 0);
+				shoot_a_pixel(&pixel_sum[idx], sqrt_ray_per_pixel, &col, d_x,
+							  d_y, objects, max_bounces, background,
+							  trace_albedo);
+				float3_mul_eq(&pixel_sum[idx], to_multiply);
+				float3_add_eq(&col.direction, delta_x);
+			}
+			float3_add_eq(&row.direction, delta_y);
+		}
+		file = fopen(filename, "wb");
+		if (file == NULL) {
+			fprintf(stderr, "Error: can't open file %s\n", filename);
+			exit(-1);
+		}
+		fprintf(file, "PF\n%d %d\n-1.0\n", width, height);
+		fwrite(pixel_sum, sizeof(Float3), total_pixel, file);
+		fclose(file);
+		fprintf(stderr, "\r%s: 1 / 1\n", filename);
+
+		filename = "normal.pfm";
+		fprintf(stderr, "%s: 0 / 1", filename);
+		to_multiply = 1.0f / ray_per_pixel;
+		row = *upper_left;
+		for (int i = 0, idx = 0; i < height; i++) {
+			Ray3 col = row;
+			for (int j = 0; j < width; j++, idx++) {
+				pixel_sum[idx] = float3_new(0, 0, 0);
+				shoot_a_pixel(&pixel_sum[idx], sqrt_ray_per_pixel, &col, d_x,
+							  d_y, objects, max_bounces, background,
+							  trace_normal);
+				float3_mul_eq(&pixel_sum[idx], to_multiply);
+				float3_add_eq(&col.direction, delta_x);
+			}
+			float3_add_eq(&row.direction, delta_y);
+		}
+		file = fopen(filename, "wb");
+		if (file == NULL) {
+			fprintf(stderr, "Error: can't open file %s\n", filename);
+			exit(-1);
+		}
+		fprintf(file, "PF\n%d %d\n-1.0\n", width, height);
+		fwrite(pixel_sum, sizeof(Float3), total_pixel, file);
+		fclose(file);
+		fprintf(stderr, "\r%s: 1 / 1\n", filename);
 	}
 
 	free(buffer);
@@ -98,13 +155,15 @@ void shoot_and_draw(const InputData* input_data) {
 inline void shoot_a_pixel(Float3* pixel_to_update, const int sqrt_ray_per_pixel,
 						  const Ray3* upper_left, const Float3* d_x,
 						  const Float3* d_y, const ObjectVec* objects,
-						  const float max_bounces, const Float3* background) {
+						  const float max_bounces, const Float3* background,
+						  Float3 (*tracer)(const Ray3*, const ObjectVec*,
+										   const int, const Float3*)) {
 	Ray3 ray = *upper_left;
 	Float3 starting_direction = ray.direction;
 	for (int ii = 0; ii < sqrt_ray_per_pixel; ii++) {
 		ray.direction = starting_direction;
 		for (int jj = 0; jj < sqrt_ray_per_pixel; jj++) {
-			Float3 light = trace_ray(&ray, objects, max_bounces, background);
+			Float3 light = tracer(&ray, objects, max_bounces, background);
 			float3_add_eq(pixel_to_update, &light);
 			float3_add_eq(&ray.direction, d_x);
 		}
@@ -145,4 +204,47 @@ inline Float3 trace_ray(const Ray3* ray, const ObjectVec* objects,
 		}
 	}
 	return light;
+}
+
+inline Float3 trace_albedo(const Ray3* ray, const ObjectVec* objects,
+						   __attribute__((unused)) const int max_bounces,
+						   __attribute__((unused)) const Float3* background) {
+	Ray3 local_ray = *ray;
+	Object* nearest_object = NULL;
+	float nearest_distance = INFINITY;
+	for (int j = 0; j < objects->size; j++) {
+		Object* object_found = &objects->ptr[j];
+		float distance_found =
+			object_intersect_distance(object_found, &local_ray);
+		if (distance_found > 0 && distance_found < nearest_distance) {
+			nearest_object = object_found;
+			nearest_distance = distance_found;
+		}
+	}
+	if (nearest_object != NULL) {
+		return nearest_object->color;
+	}
+	return float3_new(0, 0, 0);
+}
+
+inline Float3 trace_normal(const Ray3* ray, const ObjectVec* objects,
+						   __attribute__((unused)) const int max_bounces,
+						   __attribute__((unused)) const Float3* background) {
+	Ray3 local_ray = *ray;
+	Object* nearest_object = NULL;
+	float nearest_distance = INFINITY;
+	for (int j = 0; j < objects->size; j++) {
+		Object* object_found = &objects->ptr[j];
+		float distance_found =
+			object_intersect_distance(object_found, &local_ray);
+		if (distance_found > 0 && distance_found < nearest_distance) {
+			nearest_object = object_found;
+			nearest_distance = distance_found;
+		}
+	}
+	if (nearest_object != NULL) {
+		ray3_move_along(&local_ray, nearest_distance);
+		return object_normal_normalized(nearest_object, &local_ray);
+	}
+	return float3_new(0, 0, 0);
 }
--- c/src/draw.h
+++ c/src/draw.h
@@ -6,3 +6,9 @@
 void shoot_and_draw(const InputData* input_data);
 Float3 trace_ray(const Ray3* ray, const ObjectVec* objects,
 						const int max_bounces, const Float3* background);
+
+inline Float3 trace_albedo(const Ray3* ray, const ObjectVec* objects,
+						   const int max_bounces, const Float3* background);
+
+inline Float3 trace_normal(const Ray3* ray, const ObjectVec* objects,
+						   const int max_bounces, const Float3* background);
